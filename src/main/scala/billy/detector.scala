91f86ee8076ff11652a224b63f8f76a2de9e9794
Eric Christiansen <ericmartinchristiansen@gmail.com>
2013-02-18 12:18:47 -0800

    created detectors package

--- src/main/scala/billy/detector.scala
+++ src/main/scala/billy/detector.scala
@@ -40,34 +40,6 @@ object Detector {
 
 ///////////////////////////////////////////////////////////
 
-object OpenCVDetector {
-  object DENSE
-  object FAST
-  object BRISK
-  object SIFT
-  object SURF
-
-  def detectorFromEnum(detectorType: Int): Detector = new Detector {
-    override def detect = image => {
-      val matImage = OpenCVUtil.bufferedImageToMat(image)
-      val keyPoints = new MatOfKeyPoint
-      FeatureDetector.create(detectorType).detect(matImage, keyPoints)
-      keyPoints.toArray.sortBy(_.response).reverse
-    }
-  }
-
-  implicit def detector(self: DENSE.type) =
-    detectorFromEnum(FeatureDetector.DENSE)
-  implicit def detector(self: FAST.type) =
-    detectorFromEnum(FeatureDetector.FAST)
-  implicit def detector(self: BRISK.type) =
-    detectorFromEnum(FeatureDetector.BRISK)
-  implicit def detector(self: SIFT.type) =
-    detectorFromEnum(FeatureDetector.SIFT)
-  implicit def detector(self: SURF.type) =
-    detectorFromEnum(FeatureDetector.SURF)
-}
-
 case class BoundedDetector[D <% Detector](detector: D, maxKeyPoints: Int)
 
 object BoundedDetector {
@@ -88,57 +60,56 @@ object PairDetector {
   implicit class ToPairDetector[D <% Detector](self: D) extends PairDetector {
     override def detect = self.detect
 
-    override def detectPair = (homography: Homography, leftImage: BufferedImage, rightImage: BufferedImage) => {
-      val left = detect(leftImage)
-      val right = detect(rightImage)
+    override def detectPair =
+      (homography: Homography,
+        leftImage: BufferedImage,
+        rightImage: BufferedImage) => {
+        val left = detect(leftImage)
+        val right = detect(rightImage)
 
-      // Euclidean distance in pixels.
-      // TODO: Make parameter
-      val threshold = 2
+        // Euclidean distance in pixels.
+        // TODO: Make parameter
+        val threshold = 2
 
-      Util.nearestUnderWarpRemoveDuplicates(
-        threshold,
-        homography,
-        left,
-        right).sortBy(KeyPointUtil.pairQuality).reverse
-    }
+        Util.nearestUnderWarpRemoveDuplicates(
+          threshold,
+          homography,
+          left,
+          right).sortBy(KeyPointUtil.pairQuality).reverse
+      }
   }
-  
-  // This enumeration is necessary because Scala doesn't do deep searches for implicits.
-  implicit def detector(self: OpenCVDetector.DENSE.type) = self.to[Detector].to[PairDetector]
-  implicit def detector(self: OpenCVDetector.FAST.type) = self.to[Detector].to[PairDetector]
-  implicit def detector(self: OpenCVDetector.BRISK.type) = self.to[Detector].to[PairDetector]
-  implicit def detector(self: OpenCVDetector.SIFT.type) = self.to[Detector].to[PairDetector]
-  implicit def detector(self: OpenCVDetector.SURF.type) = self.to[Detector].to[PairDetector]
 }
 
-case class BoundedPairDetector[D <% PairDetector](pairDetector: D, maxKeyPoints: Int)
+///////////////////////////////////////////////////////////
 
-object BoundedPairDetector { 
-  implicit class ToPairDetector[D <% PairDetector](self: BoundedPairDetector[D]) extends PairDetector {
-    override def detect = image => self.pairDetector.detect(image).take(self.maxKeyPoints)
-    
-    override def detectPair = (homography, leftImage, rightImage) => 
-      self.pairDetector.detectPair(homography, leftImage, rightImage).take(self.maxKeyPoints)
+case class BoundedPairDetector[D <% PairDetector](
+  pairDetector: D,
+  maxKeyPoints: Int)
+
+object BoundedPairDetector {
+  implicit class ToPairDetector[D <% PairDetector](
+    self: BoundedPairDetector[D]) extends PairDetector {
+    override def detect =
+      image => self.pairDetector.detect(image).take(self.maxKeyPoints)
+
+    override def detectPair = (homography, leftImage, rightImage) =>
+      self.pairDetector.detectPair(
+        homography,
+        leftImage,
+        rightImage).take(self.maxKeyPoints)
   }
 }
 
 ///////////////////////////////////////////////////////////
 
 trait DetectorJsonProtocol extends DefaultJsonProtocol {
-  implicit val openCVDetectorDenseJsonProtocol = singletonObject(OpenCVDetector.DENSE)
-  implicit val openCVDetectorFastJsonProtocol = singletonObject(OpenCVDetector.FAST)
-  implicit val openCVDetectorBriskJsonProtocol = singletonObject(OpenCVDetector.BRISK)
-  implicit val openCVDetectorSiftJsonProtocol = singletonObject(OpenCVDetector.SIFT)
-  implicit val openCVDetectorSurfJsonProtocol = singletonObject(OpenCVDetector.SURF)
-
-  /////////////////////////////////////////////////////////
-  
-  implicit def boundedDetectorJsonProtocol[D <% Detector : JsonFormat] = jsonFormat2(BoundedDetector.apply[D])
+  implicit def boundedDetectorJsonProtocol[D <% Detector: JsonFormat] = 
+    jsonFormat2(BoundedDetector.apply[D])
 
   /////////////////////////////////////////////////////////  
-  
-  implicit def boundedPairDetectorJsonProtocol[D <% PairDetector : JsonFormat] = jsonFormat2(BoundedPairDetector.apply[D])
+
+  implicit def boundedPairDetectorJsonProtocol[D <% PairDetector: JsonFormat] = 
+    jsonFormat2(BoundedPairDetector.apply[D])
 }
 
 object DetectorJsonProtocol extends DetectorJsonProtocol
