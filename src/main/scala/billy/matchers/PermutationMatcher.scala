85763643aff4525e4e5e7ba59c3cabc422f8655f
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-08-25 14:53:23 -0700

    added permutation matchers

---
+++ src/main/scala/billy/matchers/PermutationMatcher.scala
@@ -0,0 +1,25 @@
+package billy.matchers
+
+import breeze.linalg._
+import billy._
+
+///////////////////////////////////////////////////////////
+
+/**
+ * Represents distances on permutations.
+ * 
+ */
+object PermutationMatcher {
+  /**
+   * The Cayley distance.
+   * 
+   * See "Locally Uniform Comparison Image Descriptors",
+   * Zieger et al., NIPS 2012.
+   * http://books.nips.cc/papers/files/nips25/NIPS2012_0012.pdf
+   */
+  object Cayley extends MatcherSingle[Permutation] {
+    override def distance = (left, right) => {
+      left.data.size - right.compose(left.inverse).numCycles
+    }
+  }
+}
