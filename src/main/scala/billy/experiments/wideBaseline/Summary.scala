6432422303aa716ea35fe3fc434d96402364e38a
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2013-10-13 10:37:09 -0700

    summary

---
+++ src/main/scala/billy/experiments/wideBaseline/Summary.scala
@@ -0,0 +1,17 @@
+package billy.experiments.wideBaseline
+
+import billy._
+
+import breeze.linalg._
+
+import java.io.File
+
+import st.sparse.sundry._
+
+/////////////////////////////////////////////////////////////
+
+object SummaryUtil {
+  def recognitionRate(results: Results): Double = ???
+  
+  def precisionRecall(results: Results) = ???
+}
